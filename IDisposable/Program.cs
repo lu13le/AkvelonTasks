e9664a0dc0bc86375ebc679e10b05ae0e4038c67
Luka Raicevic <raicevic12345@gmail.com>
2022-02-03 13:46:29 +0100

    IDisposable demonstration

---
+++ IDisposable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "This is line1", "This is line2", "This is line 3" };
+
+            StreamWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(@"C:\StreamWriter\name.txt");
+                foreach (var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Dispose();
+            }
+            }
+    }
+}
