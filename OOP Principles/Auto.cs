a9a16d67144a77669f30d512fad7972e504d2d3d
Luka Raicevic <raicevic12345@gmail.com>
2022-02-03 13:32:51 +0100

    Merged all task repos into one repo.

---
+++ OOP Principles/Auto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principles
+{
+    internal class Auto:Vozilo
+    {
+        #region atributi
+        private string marka;
+        private int brojSedista;
+        #endregion
+
+        #region konstruktori
+        public Auto(string marka, int brojSedista, string boja, float brzina, float maxBrzina) : base(boja, brzina, maxBrzina)
+        {
+            this.marka = marka;
+            this.brojSedista = brojSedista;
+        }
+
+        #endregion
+
+        #region Funkcije
+
+        public override void ubrzaj()
+        {
+            if (moguceUbrzanje() > 50)
+                getSetBrzina += 20;
+            else if (getSetBrzina + 10 >= getSetMaxBrzina)
+                getSetBrzina = getSetMaxBrzina;
+            else
+                getSetBrzina += 10;
+        }
+
+        public override void uspori()
+        {
+            if (brojSedista == 2)
+                getSetBrzina -= 30;
+            else
+                getSetBrzina -= 15;
+        }
+
+        public override void unos()
+        {
+            Console.WriteLine("Unesite marku auta: ");
+            marka = Console.ReadLine();
+
+            Console.WriteLine("Unesite broj sedista auta: ");
+            brojSedista = Convert.ToInt32(Console.ReadLine());
+
+            base.unos();
+
+        }
+
+        public override string ToString()
+        {
+            return $"AUTO\nMarka auta: {marka}\nBroj sedista auta:{brojSedista}\n{base.ToString()}\n";
+        }
+        #endregion
+    }
+}
---
+++ OOP Principles/Vozilo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principles
+{
+    abstract class Vozilo
+    {
+        #region atributi
+        private string boja;
+        private float brzina;
+        private float maxBrzina;
+        #endregion
+
+        #region Konstruktori
+        public Vozilo(string boja, float brzina, float maxBrzina)
+        {
+            this.boja = boja;
+            this.brzina = brzina;
+            this.maxBrzina = maxBrzina;
+        }
+        #endregion
+
+        #region polja
+
+        public float getSetBrzina
+        {
+            get { return brzina; }
+            set { brzina = value; }
+        }
+        public float getSetMaxBrzina
+        {
+            get { return maxBrzina; }
+            set { maxBrzina = value; }
+        }
+        #endregion
+
+        #region funkcije
+        public float moguceUbrzanje()
+        {
+            return maxBrzina - brzina;
+        }
+
+        public abstract void ubrzaj();
+
+        public abstract void uspori();
+
+        public virtual void unos()
+        {
+            Console.WriteLine("Unesite boju: ");
+            boja = Console.ReadLine();
+
+            Console.WriteLine("Unesite brzinu kretanja vozila: ");
+            brzina = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Unesite maksimalnu brzinu: ");
+            maxBrzina = Convert.ToSingle(Console.ReadLine());
+
+        }
+
+        public override string ToString()
+        {
+            return $"Boja vozila: {boja}\nBrzina kretanja vozila:{brzina}\nMaksimalna brzina vozila je: {maxBrzina}\n";
+        }
+        #endregion
+    }
+}
